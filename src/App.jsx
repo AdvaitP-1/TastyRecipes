ca7aa8d37c5c0d6d6698d62474f76237ba8c7abc
Advait Pandey <advait.pandey05@gmail.com>
2024-07-27 22:17:13 -0400

    as

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout.JSX';
+import SearchPage from './pages/SearchPage.JSX';
+import NutritionPage from './pages/NutritionPage.jsx';
+import NotFoundPage from './pages/NotFoundPage';
+
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<HomePage/>} />
-      <Route path='/search' element={<JobsPage/>} />
-      <Route path='/sort' element={<JobPage/>} /> 
+      <Route path='/search' element={<SearchPage/>} />
+      <Route path='/sort' element={<NutritionPage/>} /> 
       <Route path='*' element={<NotFoundPage/>} />
     </Route>
   )
